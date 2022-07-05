a46d42d27322c1a92920a938e61c84d2dff664a2
Heesoo <heesoo.p1023@gmail.com>
2022-07-05 12:01:54 +0900

    add routes for home and detail page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
 import { Navbar, Nav, Container, Row } from 'react-bootstrap';
 import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom'
 import data from './data';
 import Card from './Card';
+import NavBar from './Nav';
+import Detail from './Detail';
 
 function App() {
   const [products, setProducts] = useState(data);
@@ -17,24 +20,22 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" className='nav-bar'>
-        <Container>
-          <Navbar.Brand href="#home" className=''>Shoes Market</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Products</Nav.Link>
-            <Nav.Link href="#pricing">Cart</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <div className='main-bg'></div>
-      <Container>
-        <Row>
-          {products.map((product, i) =>
-             <Card key={i} product={product} />
-          )}
-        </Row>
-      </Container>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className='main-bg'></div>
+            <Container>
+              <Row>
+                {products.map((product, i) =>
+                  <Card key={i} product={product} />
+                )}
+              </Row>
+            </Container>
+          </>
+        } />
+        <Route path="/detail" element={<Detail />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navbar, Nav, Container, Row } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+
+
+export default function NavBar() {
+    return (
+    <Navbar bg="dark" variant="dark" className='nav-bar'>
+        <Container>
+        <Navbar.Brand href="#home" className=''>Shoes Market</Navbar.Brand>
+        <Nav className="me-auto">
+            <Link to ="/" className="nav-link">Home</Link>
+            <Link to ="/detail" className="nav-link">Products</Link>
+        </Nav>
+        </Container>
+    </Navbar>
+    )
+}
