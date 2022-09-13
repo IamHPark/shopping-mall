c7ec6bc36bfdeb4d9d9e217c89867c7f47a3c117
Heesoo <heesoo.p1023@gmail.com>
2022-09-12 20:53:35 -0700

    install redux and store state

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Container, Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import './Cart.css'
 
 export default function Cart() {
-    return (
 
+    const stock = useSelector( state => state.stock)
+    console.log(stock)
+
+    return (
+        <Container>
+            <Table striped bordered hover className="cart" >
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Change</th>
+            </tr>
+            </thead>
+            <tbody>
+                {stock.map((item, i) =>
+                    <tr>
+                        <td>{i+1}</td>
+                        <td>{item.name}</td>
+                        <td>{item.count}</td>
+                        <td><button>+</button></td>
+                    </tr>
+                )}
+            </tbody>
+        </Table>
+      </Container>
     )
 }
---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const stock = createSlice({
+    name: 'stock',
+    initialState: [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+      ]
+})
+
+export default configureStore({
+    reducer: {
+        stock: stock.reducer
+    }
+})
